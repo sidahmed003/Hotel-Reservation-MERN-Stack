17aa835f9a1aa099f915233bcf9a426a46bfd1ce
sidahmed zerkani <sidahmedzerkani@gmail.com>
2025-04-04 23:05:39 +0100

    Reservation Panel added successfully

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,8 +1,24 @@
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react';
+import useFetch from '../../hooks/useFetch';
 
 const Reserve = ({setOpen, hotelId}) => {
+    const [selectedRooms, setSelectedRooms] = useState([]);
+    const { data, loading, error } = useFetch(`http://localhost:5000/api/hotels/room/${hotelId}`);
+
+    const handleSelect = (e) => {
+        const checked = e.target.checked;
+        const value = e.target.value;
+        setSelectedRooms(
+            checked 
+              ? [...selectedRooms, value]
+              : selectedRooms.filter((item) => item !== value) 
+        )
+    }
+
+    console.log(selectedRooms);
+
   return (
     <div classNmae="reserve">
         <div className="rContainer">
@@ -12,6 +28,24 @@ const Reserve = ({setOpen, hotelId}) => {
               onClick={() => setOpen(false)} 
             />
             <span>Select Your Rooms</span>
+            {data.map((item) => (
+                <div className="rItem">
+                    <div className="rItemInfo">
+                        <div className="rTitle">{item.title}</div>
+                        <div className="rDesc">{item.desc}</div>
+                        <div className="rMax">
+                            Max People: <b>{item.maxPeople}</b>
+                        </div>
+                        <div className="rPrice">{item.price}</div>
+                    </div>
+                    {item.roomNumbers.map((roomNumber) => (
+                        <div className="room">
+                            <label>{roomNumber.number}</label>
+                            <input type="checkbox" value={roomNumber._id} onChange={handleSelect}/>
+                        </div>
+                    ))}
+                </div>
+            ))}       
         </div>
     </div>
   )
